41d13a8dfa3942a9ea78e3d456adc51cd0d2a4b3
glenn-kerselaers <glenn.kerselaers@gmail.com>
2023-02-18 00:41:38 +0100

    Autocommit from 2023-02-18 00:41:38
    Klipper on commit: dab00ae
    
    Moonraker on commit: 0a811b9
    
    Mainsail version: v2.4.1
    
    Fluidd version: v1.23.1

--- script/dropProbe.sh
+++ script/dropProbe.sh
@@ -4,4 +4,5 @@
   git remote add probe-drop-first https://github.com/hifihedgehog/klipper.git
   git fetch probe-drop-first
   git cherry-pick ad23f41ce542b8a637ef91a57dad5d4e2ee7b6f1
+  sed -i 's/TRSYNC_TIMEOUT = 0.025/TRSYNC_TIMEOUT = 0.05/g' ~/klipper/klippy/mcu.py
   sudo service klipper restart
