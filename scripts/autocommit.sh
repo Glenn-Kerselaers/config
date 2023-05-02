ff211b3c411a0dd7c322e0173dad5a7daf3b3b36
glenn-kerselaers <glenn.kerselaers@gmail.com>
2023-05-02 22:35:15 +0200

    Autocommit from 2023-05-02 22:35:15
    Klipper on commit: 1a24e7c
    
    Moonraker on commit: 6c7dfe5
    
    Mainsail version: v2.5.1

--- scripts/autocommit.sh
+++ scripts/autocommit.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+#######################################################################
+## NOTE: This script originates from here but I tweaked the pull     ##
+## command, changed default location for backup, and added a comment ##
+## for reference later.                                              ##
+#######################################################################
+
 #####################################################################
 ### Please set the paths accordingly. In case you don't have all  ###
 ### the listed folders, just keep that line commented out.        ###
 #####################################################################
 ### Path to your config folder you want to backup
 config_folder=~/printer_data/config
+
+# NOTE: The above should work for just about everyone, but a somewhat
+# recent update to moonraker changed paths, etc. You can run the 
+# provided moonraker script 'data-path-fix.sh' to fix/update
+# older installs
 
 ### Path to your Klipper folder, by default that is '~/klipper'
 klipper_folder=~/klipper
@@ -18,6 +29,10 @@ mainsail_folder=~/mainsail
 
 ### Path to your Fluidd folder, by default that is '~/fluidd'
 #fluidd_folder=~/fluidd
+
+### The branch of the repository that you want to save your config
+### By default that is 'master'
+branch=V24
 
 #####################################################################
 #####################################################################
@@ -49,13 +64,20 @@ grab_version(){
   fi
 }
 
+# To fully automate this and not have to deal with auth issues, generate a legacy token on Github
+# then update the command below to use the token. Run the command in your base directory and it will
+# handle auth. This should just be executed in your shell and not committed to any files or
+# Github will revoke the token. =)
+# git remote set-url origin https://XXXXXXXXXXX@github.com/EricZimmerman/Voron24Configs.git/
+# Note that that format is for changing things after the repository is in use, vs initially
+
 push_config(){
   cd $config_folder
-  git pull
+  git pull origin $branch
   git add .
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push
+  git push origin $branch
 }
 
 grab_version
